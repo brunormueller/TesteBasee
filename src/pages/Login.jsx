af19a126d41ddb142cfcc92f3be559c043744bf6
Bruno Roberto Mueller - External <e-brunor@weg.net>
2022-05-10 15:29:57 -0300

    atualizado

---
+++ src/pages/Login.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import Routes from "../Routes";
+import axios from "axios";
+import '../styles.css';
+import { Link } from 'react-router-dom';
+import { LayoutComponents } from "../components/LayoutComponents";
+import { history } from "../history";
+
+export const Login = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+//AUTENTICAÇÃO LOGIN 
+    const handleSubmit = values =>{
+        axios.post('/api/Usuario', values)
+            .then (resp =>{
+                const {data} = resp
+                if (data) {
+                    localStorage.setItem('app-token', data)
+                    history.push('/home')
+                }
+            })
+    }
+    return (
+        <LayoutComponents>
+            <form className="login-form">
+                <span className="login-form-title">Bem Vindo!</span>
+                <span className="login-form-title">
+
+                </span>
+                <div className="wrap-input">
+                    <input className="has-val input" type="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)} />
+                    <span className="focus-input" data-placeholder="Email"></span>
+                </div>
+                <div className="wrap-input">
+                    <input className="has-val input" type="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)} />
+                    <span className="focus-input" data-placeholder="Password"></span>
+                </div>
+                < div className="container-login-form-btn">
+                    <button className="login-form-btn">Login</button>
+                </div>
+                <div className="text-center">
+                    <span className="txt1">Não possui conta?</span>
+
+                    <Link className="txt2" to="/register">Criar Conta</Link>
+                </div>
+            </form>
+        </LayoutComponents>
+
+    );
+
+}
